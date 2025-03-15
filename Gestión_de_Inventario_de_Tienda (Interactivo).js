70133a2ede2c8ca58ebb69ab7a78da0a21c862dc
SANTTYAGO <Santyagoelpro@gmail.com>
2025-03-14 20:45:48 -0500

    Se define la clase Producto

--- Gestión_de_Inventario_de_Tienda (Interactivo).js
+++ Gestión_de_Inventario_de_Tienda (Interactivo).js
@@ -1,1 +1,10 @@
- 
+const readline = require('readline');
+
+// Aqui se define la clase Producto
+class Producto {
+    constructor(nombre, precio, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
