b83c7b65f6232a9a385b24d501dd33a13393e355
SANTTYAGO <Santyagoelpro@gmail.com>
2025-03-14 20:36:59 -0500

    Método para actualizar la cantidad de un producto

--- Gestión_de_Inventario_de_Tienda (Basico).js
+++ Gestión_de_Inventario_de_Tienda (Basico).js
@@ -12,10 +12,21 @@ class Tienda {
     constructor() {
         this.productos = [];
     }
-    
-        // Método para agregar un producto
-        agregarProducto(producto) {
-            this.productos.push(producto);
+
+    // Método para agregar un producto
+    agregarProducto(producto) {
+        this.productos.push(producto);
+    }
+
+    // Método para actualizar la cantidad de un producto
+    actualizarCantidad(nombre, nuevaCantidad) {
+        const producto = this.productos.find(p => p.nombre === nombre);
+        if (producto) {
+            producto.cantidad = nuevaCantidad;
+        } else {
+            console.log(`Producto ${nombre} no encontrado.`);
         }
+    }
+
 }
 
